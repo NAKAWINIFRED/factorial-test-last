f897bc8daf05a236297f48d55624b0d47c9f6ecd
NAKAWINIFRED <winifrednakawunde07@gmail.com>
2024-07-09 00:47:32 +0300

    add tests

--- test/test.js
+++ test/test.js
@@ -1,1 +1,22 @@
+// Kalumba Isaac//
+// Nakawunde Winifred//
 
+import { expect } from "chai";
+import { factorial } from "../factorial.js";
+
+describe("Factorial:test last", () => {
+  it("should return 1 for factorial of 0", () => {
+    expect(factorial(0)).to.equal(1);
+  });
+
+  it("should return 1 for factorial of 1", () => {
+    expect(factorial(1)).to.equal(1);
+  });
+
+  it("should return 120 for factorial of 3", () => {
+    expect(factorial(3)).to.equal(6);
+  });
+  it("should return 120 for factorial of 4", () => {
+    expect(factorial(4)).to.equal(24);
+  });
+});
