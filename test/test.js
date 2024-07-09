ac1b691d4cd7f4015263b85e953cb3626685b51a
Isaac Kalumba <ikalumba221@gmail.com>
2024-07-09 22:59:45 +0300

    refactor

--- test/test.js
+++ test/test.js
@@ -1,22 +1,25 @@
+// --TEAM MEMBERS--
 // Kalumba Isaac//
 // Nakawunde Winifred//
 
-import { expect } from "chai";
+import { assert } from "chai";
 import { factorial } from "../factorial.js";
+import { describe, it } from 'mocha';
+
 
 describe("Factorial:test last", () => {
   it("should return 1 for factorial of 0", () => {
-    expect(factorial(0)).to.equal(1);
+    assert.equal(factorial(0), 1);
   });
-
+  
   it("should return 1 for factorial of 1", () => {
-    expect(factorial(1)).to.equal(1);
+    assert.equal(factorial(1), 1);
   });
-
-  it("should return 120 for factorial of 3", () => {
-    expect(factorial(3)).to.equal(6);
+  
+  it("should return 6 for factorial of 3", () => {
+    assert.equal(factorial(3), 6);
   });
-  it("should return 120 for factorial of 4", () => {
-    expect(factorial(4)).to.equal(24);
+  it("should return 24 for factorial of 4", () => {
+    assert.equal(factorial(4), 24);
   });
 });
